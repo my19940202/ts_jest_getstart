d72a60ee73181d94b62c5624b93b7059913ac8a5
xibobo <xishengbo@baidu.com>
2019-07-24 12:38:52 +0800

    jest get started

--- __tests__/xsb.test.ts
+++ __tests__/xsb.test.ts
@@ -2,25 +2,25 @@
 
 console.log("what the fuck ");
 test("two plus two is four", () => {
-  expect(2 + 2).toBe(4);
+    expect(2 + 2).toBe(4);
 });
 
 // toEqual的使用
 test("object assignment", () => {
-  const data = { one: 1 };
-  data["two"] = 2;
-  expect(data).toEqual({ one: 1, two: 2 });
+    const data: any = { one: 1 };
+    data["two"] = 2;
+    expect(data).toEqual({ one: 1, two: 2 });
 });
 
 // not.toBe
-// test('adding positive numbers is not zero', () => {
-//     for (let a = 1; a < 10; a++) {
-//         for (let b = 1; b < 10; b++) {
-//             console.log('run', a, b);
-//             expect(a + b).not.toBe(0);
-//         }
-//     }
-// });
+test('adding positive numbers is not zero', () => {
+    for (let a = 1; a < 10; a++) {
+        for (let b = 1; b < 10; b++) {
+            console.log('run', a, b);
+            expect(a + b).not.toBe(0);
+        }
+    }
+});
 
 // 函数callback的时候如何测试
 // test('the data is peanut butter', done => {
